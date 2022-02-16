569311567391e6085a137cf51d68f17619d181f4
Thomas Gingele <thomas@gingele.de>
2022-02-16 19:05:26 +0100

    Update doc comments

--- snack_machine/tui_engine.py
+++ snack_machine/tui_engine.py
@@ -6,10 +6,10 @@ from utility import check_args,\
                     list_line_length
 
 
-def required_length(title: str, data: dict, options: dict):
+def required_length(title: str, data: dict, options: list):
     final_len = len(title)
     data_len = dict_line_length(data)
-    options_len = list_line_length(list(options.keys()))
+    options_len = list_line_length(options)
 
     if final_len < data_len:
         final_len = data_len
@@ -36,33 +36,14 @@ class TuiEngine:
              "none": "\033[0m"}
 
     selected_index = 0
-    options_list = list
 
     title = ""
     data = dict
-    options = dict
+    options = list
 
     line_length = 0
 
-    def execute_selected_item(self):
-        if not self.selected_index >= len(self.options_list):
-            self.options_list[self.selected_index]()
-
-    def selection_up(self):
-        if not self.selected_index == 0:
-            self.selected_index -= 1
-            self.render(self.title,
-                        self.data,
-                        self.options)
-
-    def selection_down(self):
-        if not self.selected_index == len(self.options_list)-1:
-            self.selected_index += 1
-            self.render(self.title,
-                        self.data,
-                        self.options)
-
-    def render(self, title: str, data: dict, options: dict):
+    def render(self, title: str, data: dict, options: list):
         """
         Converts input into a box-like structure
         :param title: The title of the structure
@@ -83,7 +64,6 @@ class TuiEngine:
         title_render = f"| {title}{(self.line_length-len(title))*' '} |"
         data_render = self._render_data()
         options_render = self._render_options()
-        self.options_list = list(options.values())
 
         _display(title_render,
                  data_render,
@@ -100,7 +80,7 @@ class TuiEngine:
 
     def _render_options(self) -> str:
         options_render = ""
-        for index, entry in enumerate(self.options.keys()):
+        for index, entry in enumerate(self.options):
             if index == self.selected_index:
                 options_render += f"->{self.color['green']}{entry}{self.color['none']}" \
                                   f"{(self.line_length-len(entry))*' '} |\n"
@@ -108,40 +88,3 @@ class TuiEngine:
                 options_render += f"| {entry}{(self.line_length-len(entry))*' '} |\n"
 
         return options_render
-
-
-if __name__ == "__main__":
-    print("Running tests for tui_engine.py\n"
-          "If you want to start the full application, please use 'python snack_machine.py'")
-    engine = TuiEngine()
-
-    def function_one():
-        print("function one executed!")
-
-    def function_two():
-        print("function two executed!")
-
-    def function_three():
-        print("function three executed!")
-
-    engine.render("Test Run", {"Information": "Test Run", "Length Test": "This line is incredibly long because "
-                                                                         "i want too see how my application deals "
-                                                                         "with it, lets go and test it out :D",
-                               "Symbol Test": "@ſ€¶ŧ←↓→øþſðđŋħł»«¢„“”µ·…–′¹²³¼½¬{[]}\\"},
-                  {"Execute Function 1": function_one,
-                   "Execute Function 2": function_two,
-                   "Execute Function 3": function_three})
-
-    print("Executing options\n"
-          "Testing multiple selection scenarios\n"
-          "Execution might seem out of order")
-    engine.execute_selected_item()
-    engine.selection_up()
-    engine.selection_down()
-    engine.execute_selected_item()
-    engine.selection_down()
-    engine.execute_selected_item()
-    engine.selection_down()
-    engine.execute_selected_item()
-    engine.selection_up()
-    engine.execute_selected_item()
--- snack_machine/utility.py
+++ snack_machine/utility.py
@@ -2,6 +2,14 @@ from exceptions import InvalidArgumentException
 
 
 def dict_line_length(element: dict) -> int:
+    """
+    Calculates the length of the longest key/value-pair in a dictionary.\n
+    The length will be calculated from this format:\n
+    "<key>: <value>".
+
+    :param element: The dictionary the calculation will be performed on.
+    :return:
+    """
     element_len = 0
     for key, entry in element.items():
         line_len = len(f"{key}: {entry}")
@@ -11,6 +19,12 @@ def dict_line_length(element: dict) -> int:
 
 
 def list_line_length(element: list):
+    """
+    Calculates the length of the longest entry in a list.
+
+    :param element: The list the calculation will be performed on.
+    :return:
+    """
     element_len = 0
     for entry in element:
         line_len = len(entry)
@@ -20,13 +34,13 @@ def list_line_length(element: list):
 
 
 def check_args(*args):
+    """
+    Checks if any parameter is None.\n
+    Throws InvalidArgumentException if yes.
+
+    :param args:
+    :return:
+    """
     for arg in args:
         if arg is None:
             raise InvalidArgumentException("A parameter was 'None'.")
-
-
-if __name__ == "__main__":
-    print("Testing 'check_args'\n"
-          "Expected Result: throws InvalidArgumentException\n"
-          "Actual result:\n")
-    check_args(None)
