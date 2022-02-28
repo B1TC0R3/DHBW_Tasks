d3dffb307a590dce24ab567aecd9e52e4437ce27
Thomas Gingele <thomas@gingele.de>
2022-02-28 12:01:36 +0100

    Add acccount_manager.py

---
+++ snack_machine/account_manager.py
@@ -0,0 +1,67 @@
+"""
+Module containing some functions
+to easily create and log in to accounts.
+"""
+import json
+import os
+from account import Account
+from exceptions import InvalidLoginError
+
+def create_account() -> Account:
+    """
+    Creates a new object of type account.
+
+    :returns: Account
+    """
+    os.system("clear")
+    acc_name = input("Enter account name:")
+    acc_pwd = input("Enter account pwd:")
+
+    acc = Account(acc_name, acc_pwd)
+    save(acc)
+    return acc
+
+
+def login(name: str, pwd: str) -> Account:
+    """
+    Logs into an existing account.
+
+    :param name: The name of the account.
+    :param pwd: The password of the account.
+    :returns: Account
+    """
+    if not os.path.isfile(f"{_file_dir()}/{name}.json"):
+        raise InvalidLoginError
+
+    with open(f"{_file_dir()}/{name}.json", "r", encoding="utf-8") as file:
+        acc_dict = json.load(file)
+
+    if not pwd == acc_dict["pwd"]:
+        raise InvalidLoginError
+
+    acc = Account(acc_dict["name"], acc_dict["pwd"])
+    acc.add_balance(float(acc_dict["balance"]))
+    return acc
+
+def save(acc: Account):
+    """
+    Saves an account to a json file.
+    The directory is: "./.accounts/"
+
+    :param acc: The account that will be saved.
+    :returns: None
+    """
+    json_dict = acc.to_json()
+
+    access = "w" if os.path.isfile(f"{_file_dir()}/{acc.get_name()}.json") else "x"
+    with open(f"{_file_dir()}/{acc.get_name()}.json", access, encoding="utf-8") as file:
+        json.dump(json_dict, file)
+
+
+def _file_dir() -> str:
+    """
+    The directory the account files ought to be stored in.
+
+    :returns: str
+    """
+    return "./.accounts"
