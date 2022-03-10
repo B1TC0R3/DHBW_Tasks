b43ff64dfcf5db1bc33f1f57249d431e263bd452
Thomas Gingele <thomas@gingele.de>
2022-03-10 10:41:39 +0100

    Reads values from file now

--- src/tmptbl.c
+++ src/tmptbl.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 const char* const GREEN = "\033[32m";
 const char* const RED = "\033[31m";
@@ -6,25 +8,36 @@ const char* const CYAN = "\033[36m";
 const char* const NONE = "\033[0m";
 
 struct month {
-	char name[3];
+	char* name;
 	float tmax;
 	float tmin;
 };
 
 void loadYear(struct month year[12]) 
 {
-	year[0] = (struct month){"Jan", 2.1f, -2.8f };
-	year[1] = (struct month){"Feb", 3.5f, -2.4f};
-	year[2] = (struct month){"Mar", 7.4f, 0.2f};
-	year[3] = (struct month){"Apr", 11.7f, 3.0f};
-	year[4] = (struct month){"May", 16.8f, 7.2f};
-	year[5] = (struct month){"Jun", 20.0f, 10.5f};
-	year[6] = (struct month){"Jul", 21.8f, 12.3f};
-	year[7] = (struct month){"Aug", 21.7f, 12.2f};
-	year[8] = (struct month){"Sep", 18.3f, 9.6};
-	year[9] = (struct month){"Okt", 13.2f, 5.9f};
-	year[10] = (struct month){"Nov", 7.0f, 1.7f};
-	year[11] = (struct month){"Dez", 3.4f, -1.3f};
+	FILE* file = fopen("tmp.csv", "r");
+	int buffer = 32;
+	char line[buffer];
+	const char* delimiter = ",";
+
+	if(file == NULL){
+		printf("%sFailed to open file!%s\n", RED, NONE);
+		return;
+	}
+	
+	int counter = 0;
+	while(fgets(line, buffer, file)) {
+		char* ldup = strdup(line);
+
+		char* name = strsep(&ldup, delimiter);
+		float tmax = atof(strsep(&ldup, delimiter));
+		float tmin = atof(ldup);
+
+		year[counter] = (struct month){name, tmax, tmin};
+		counter++;
+	}
+
+	fclose(file);
 }
 
 void printNames(struct month year[12]) {
