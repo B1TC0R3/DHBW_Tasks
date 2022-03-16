8447344c3ae673b6bac7d61f310bfdcc7a2e004b
Thomas Gingele <thomas@gingele.de>
2022-03-16 15:10:16 +0100

    Add file line counter

--- src/grouper.c
+++ src/grouper.c
@@ -1,12 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct student{
+	char* name;
+	char* email;	
+};
+
+int countLines(char* filePath) {
+	FILE* file = fopen(filePath, "r");
+	int lines = 0;
+	char ch;
+
+	while(!feof(file)) {
+		ch = fgetc(file);
+		if (ch == '\n') {
+			lines++;
+		}
+	} 
+
+	return lines;
+}
+
 int main(int argc, char** argv) {
-	if (argc != 2)
+	if (argc != 3) {
 		printf("\033[31mInvalid parameters!\033[0m\n");
+		printf("Found %i, needed 2.\n", argc);
+		return -1;
+	}
 	
 	char* filePath = argv[0];
 	int groupSize = atoi(argv[1]);
+	
+	int lineSize = countLines(filePath);
+	printf("Students found in file: %i\n", lineSize);
 
 	return 0;
 }
