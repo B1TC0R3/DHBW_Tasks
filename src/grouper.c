9f219ad484bbad065d807ea36e795f67171f9e34
Thomas Gingele <thomas@gingele.de>
2022-03-16 14:51:47 +0100

    Convert paramters to useable variables

--- src/grouper.c
+++ src/grouper.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char** argv) {
 	if (argc != 2)
 		printf("\033[31mInvalid parameters!\033[0m\n");
+	
+	char* filePath = argv[0];
+	int groupSize = atoi(argv[1]);
+
 	return 0;
 }
